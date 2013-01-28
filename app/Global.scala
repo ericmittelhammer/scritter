a538fcfc2d060b1d512be90f370d08a66f409cbe
Eric Mittelhammer <eric@gawker.com>
2013-01-28 17:18:13 -0500

    added routes

--- app/Global.scala
+++ app/Global.scala
@@ -1,4 +1,4 @@
-package global
+package global 
 
 import play.api._
 import play.api.db._
@@ -8,10 +8,17 @@ import scala.slick.driver.H2Driver
 
 import models.Tables
 import services.SlickUserService
+import controllers.Application
 
 object Global extends GlobalSettings {
 
 	lazy val ds = DB.getDataSource()
 
 	lazy val userService = new SlickUserService(ds) with H2Driver with Tables
+
+	/*override def getControllerInstance[A](controllerClass: Class[A]) = {
+	    controllerClass match {
+	    	case _:Class[Application] => new Application(new SlickUserService(DB.getDataSource()) with H2Driver with Tables)
+	    	}
+	  }*/
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,28 +2,42 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.Play.current
+
+import global.Global
 
 import models.User
 import models.AnonymousUser
 
-import services.UserServiceProvider
+import services.UserService
 
 object Application extends Controller {
 
-	def userService = current.plugin(classOf[UserServiceProvider]).get.userService
+	def userService = Global.userService
+
+	def registrationForm = Form(tuple("username" -> text, 
+									"email" -> email, 
+									"password" -> text, 
+									"passwordVerify" -> text ))
 
 	def withUser(f: User => Request[AnyContent] => Result) = {
-	  Action { request =>
-	    request.session.get("userId").flatMap(id => userService.findById(id.toInt)) match {
-	    	case Some(user) => f(user)(request)
-	    	case _ => f(AnonymousUser)(request)
-	    	}     
+		Action { request =>
+			request.session.get("userId").flatMap(id => userService.findById(id.toInt)) match {
+				case Some(user) => f(user)(request)
+				case _ => f(AnonymousUser)(request)
+			}     
 		}
 	}
 
-  def index = withUser { user => request =>
-    Ok(views.html.index(user))
-  }
-  
+	def index = withUser { user => request =>
+		if(user == AnonymousUser) Ok(views.html.index(user, registrationForm))
+		else Ok(views.html.homepage(user))
+	}
+
+	def register = withUser { user => request =>
+		Ok(views.html.homepage(user))
+	}
+
 }
